0652b9613dd9f0d36664835736d2e0ca2480016f
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-12-01 23:16:15 -0400

    Initial commit

---
+++ ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/SsdTbwMonitoringApiApplication.java
@@ -0,0 +1,13 @@
+package com.cristianml.SSDMonitoringApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdTbwMonitoringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdTbwMonitoringApiApplication.class, args);
+	}
+
+}
---
+++ ssd-tbw-monitoring-api/src/test/java/com/cristianml/SSDMonitoringApi/SsdTbwMonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianml.SSDMonitoringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdTbwMonitoringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

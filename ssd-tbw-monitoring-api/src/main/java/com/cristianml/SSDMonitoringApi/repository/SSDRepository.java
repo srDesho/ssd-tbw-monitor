f71ccb7b847c27dbbb329f62f95c5c95ae27982c
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-12-10 18:55:43 -0400

    SSDService created

--- ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/repository/SSDRepository.java
+++ ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/repository/SSDRepository.java
@@ -1,13 +1,13 @@
 package com.cristianml.SSDMonitoringApi.repository;
 
-import com.cristianml.SSDMonitoringApi.domain.SSD;
+import com.cristianml.SSDMonitoringApi.domain.SSDEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SSDRepository extends JpaRepository<SSD, Long> {
+public interface SSDRepository extends JpaRepository<SSDEntity, Long> {
 
-    SSD findByModel(String model);
+    SSDEntity findByModel(String model);
     boolean existsByModel(String model);
 
 }
--- ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/repository/TbwRecordRepository.java
+++ ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/repository/TbwRecordRepository.java
@@ -1,13 +1,13 @@
 package com.cristianml.SSDMonitoringApi.repository;
 
-import com.cristianml.SSDMonitoringApi.domain.TbwRecord;
+import com.cristianml.SSDMonitoringApi.domain.TbwRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
 @Repository
-public interface TbwRecordRepository extends JpaRepository<TbwRecord, Long> {
+public interface TbwRecordRepository extends JpaRepository<TbwRecordEntity, Long> {
 
     boolean existsByDateAndSsd_id(LocalDate date, Long ssdId);
 

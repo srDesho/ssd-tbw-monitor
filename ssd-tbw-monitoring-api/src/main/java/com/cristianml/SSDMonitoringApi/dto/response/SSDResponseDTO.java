a74c04eda20cd03af72e00de385821f996391a76
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-01-01 19:43:57 -0400

    Adding formatter date

--- ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/dto/response/SSDResponseDTO.java
+++ ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/dto/response/SSDResponseDTO.java
@@ -2,19 +2,25 @@ package com.cristianml.SSDMonitoringApi.dto.response;
 
 import com.cristianml.SSDMonitoringApi.domain.TbwRecordEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SSDResponseDTO {
 
     private Long id;
     private String model;
     private Long capacityGB;
     private LocalDateTime registrationDate;
+    private String formattedDateTime;
 
 }
--- ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/mapper/SSDMapper.java
+++ ssd-tbw-monitoring-api/src/main/java/com/cristianml/SSDMonitoringApi/mapper/SSDMapper.java
@@ -2,6 +2,8 @@ package com.cristianml.SSDMonitoringApi.mapper;
 
 import com.cristianml.SSDMonitoringApi.domain.SSDEntity;
 import com.cristianml.SSDMonitoringApi.dto.response.SSDResponseDTO;
+import com.cristianml.SSDMonitoringApi.service.impl.HardwareServiceImpl;
+import com.cristianml.SSDMonitoringApi.utilities.Utilities;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,10 @@ public class SSDMapper {
     private final ModelMapper modelMapper;
 
     public SSDResponseDTO toResponseDTO(SSDEntity ssdEntity) {
-        return modelMapper.map(ssdEntity, SSDResponseDTO.class);
+        SSDResponseDTO ssdResponseDTO = modelMapper.map(ssdEntity, SSDResponseDTO.class);
+        ssdResponseDTO.setFormattedDateTime(Utilities.formatLocalDateTime(ssdEntity.getRegistrationDate()));
+        System.out.println("###################################" + ssdResponseDTO.getFormattedDateTime());
+        return ssdResponseDTO;
     }
 
     public List<SSDResponseDTO> toSSDResponseDTOList(List<SSDEntity> ssdList) {
